46f3bd1d49ef1465b60f6e79b5df45fa34f0c155
Kateryna <kunieva.kateryna@gmail.com>
2026-01-30 13:10:47 +0100

    Added missed services and passwords generating

--- scripts/create-secrets.sh
+++ scripts/create-secrets.sh
@@ -17,9 +17,18 @@ NAMESPACE="anime-project"
 
 # Default values
 POSTGRES_USER="${POSTGRES_USER:-user}"
-POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-$(openssl rand -base64 16)}"
+POSTGRES_PASSWORD="${POSTGRES_PASSWORD:-123456789}"
+
 MONGO_USER="${MONGO_USER:-root}"
 MONGO_PASSWORD="${MONGO_PASSWORD:-$(openssl rand -base64 16)}"
+
+MAIL_USER="${MAIL_USERNAME:-user}"
+MAIL_PASSWORD="${MAIL_PASSWORDWORD:-$(openssl rand -base64 16)}"
+
+ADMIN_EMAIL="${ADMIN_EMAIL:admin@animedomain.com}"
+
+RABBIT_USER="${RABBIT_USER:-rabbit}"
+RABBIT_PASSWORD="${RABBIT_PASSWORD:-$(openssl rand -base64 16)}"
 
 # PostgreSQL Secret
 echo "Creating postgresql-secret in namespace $NAMESPACE..."
@@ -36,3 +45,24 @@ kubectl create secret generic mongodb-secret \
   --from-literal=password="$MONGO_PASSWORD" \
   -n "$NAMESPACE" \
   --dry-run=client -o yaml | kubectl apply -f -
+
+# Rabbit MQ Secret
+echo "Creating rabbitmq-secret..."
+kubectl create secret generic rabbitmq-secret \
+  --from-literal=username="$RABBITMQ_USER" \
+  --from-literal=password="$RABBITMQ_PASSWORD" \
+  -n "$NAMESPACE" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+# Mail Secret
+echo "Creating mail-secret..."
+kubectl create secret generic mail-secret \
+  --from-literal=username="$MAIL_USER" \
+  --from-literal=password="$MAIL_PASSWORD" \
+  -n "$NAMESPACE" \
+  --dry-run=client -o yaml | kubectl apply -f -
+
+# Admin email
+kubectl create secret generic mail-secret \
+  --from-literal=admin-email="$ADMIN_EMAIL" \
+  -n "$NAMESPACE" --dry-run=client -o yaml | kubectl apply -f -
